2c820183544b2b7b87aab99ccd5ebb4cbe60cbf7
Thet Zaw <39977377+thetz@users.noreply.github.com>
2021-05-16 12:34:47 -0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,6 +5,19 @@
 //   - One for activation ( check out MDN's clients.claim() for this step )
 //   - One for fetch requests
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function () {
+    navigator.serviceWorker.register('./sw.js').then(function (registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function (err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
+
+
 
 //installation
 
